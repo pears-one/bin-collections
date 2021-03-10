3aa21068084407153f01ea8abd7ca43305b3f17a
Evan Pearson <evan.pearson@thehutgroup.com>
2021-03-10 10:52:33 +0000

    format dates nicely

--- src/model/alert.py
+++ src/model/alert.py
@@ -1,4 +1,5 @@
 from model.person import Person
+from datetime import date
 from collection.bin_day import BinDay
 from typing import List
 
@@ -16,7 +17,8 @@ class Alert:
 
     def get_message(self):
         bins = self.__bin_day.get_bin_types()
-        return f"Hi {self.get_person().get_name()}, the {self.__nice_join(bins)} bins are getting collected on {self.get_date()}."
+        date_string = self.__format_date(self.get_date())
+        return f"Hi {self.get_person().get_name()}, the {self.__nice_join(bins)} bins are getting collected on {date_string}."
 
     @staticmethod
     def __nice_join(things: List[str]) -> str:
@@ -24,3 +26,9 @@ class Alert:
             last_thing = things.pop()
             things.append(f"{things.pop()} and {last_thing}")
         return ', '.join(things)
+
+    @staticmethod
+    def __format_date(d: date) -> str:
+        n = d.day
+        ordinal = str(n) + {1: 'st', 2: 'nd', 3: 'rd'}.get(10 <= (n % 100) <= 20 and n or n % 10, 'th')
+        return d.strftime(f'%A, {ordinal} %B')
