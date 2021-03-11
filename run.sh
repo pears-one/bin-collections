b57a845a60f5c2c51e6c0b92b778769d3d9e97b2
Evan Pearson <evan.pearson@thehutgroup.com>
2021-03-11 20:31:21 +0000

    allow delay to be specified in script argument

--- run.sh
+++ run.sh
@@ -1,44 +1,61 @@
-MV_MSG='missing variable:'
+USAGE='./$0 [live] [DELAY_IN_DAYS]'
 
-if [ "$BIN_VENV" == "" ]; then
-  echo "$MV_MSG BIN_VENV"
-  exit 1
-fi
+function check_variable {
+  var_name=$1
+  var_value=${!var_name}
+  MV_MSG='missing variable:'
+  if [ "$var_value" == "" ]; then
+    echo "$MV_MSG $var_name"
+    deactivate 2> /dev/null
+    exit 1
+  fi
+}
+
+function is_int {
+  [[ $# -ne 1 || $1 =~ [0-9]+ ]] && return
+}
+
+function is_live {
+  [[ $1 == "live" ]] && return
+}
+
+function process_arguments {
+  case $# in
+    2)
+      if is_int $2; then
+        DELAY_IN_DAYS=$2
+      fi
+      if is_live $1; then
+        MODE="live"
+      fi
+      ;;
+    1)
+      if is_int $1; then
+        DELAY_IN_DAYS=$1
+        return
+      fi
+      if is_live $1; then
+        MODE="live"
+      fi
+    ;;
+  esac
+}
+
+check_variable BIN_VENV
 
 . $BIN_VENV/bin/activate
 
 # THESE VARIABLES SHOULD BE SET IN THE VIRTUAL ENVIRONMENT ACTIVATE SCRIPT
-if [ "$ACCOUNT_SID" == "" ]; then
-  echo "$MV_MSG ACCOUNT_SID"
-  deactivate
-  exit 1
-fi
+check_variable ACCOUNT_SID
+check_variable AUTH_TOKEN
+check_variable NUMBER
+check_variable DELAY_IN_DAYS
+check_variable DB_ADDRESS
 
-if [ "$AUTH_TOKEN" == "" ]; then
-  echo "$MV_MSG AUTH_TOKEN"
-  deactivate
-  exit 1
-fi
-
-if [ "$NUMBER" == "" ]; then
-  echo "$MV_MSG NUMBER"
-  deactivate
-  exit 1
-fi
-
-if [ "$DELAY_IN_DAYS" == "" ]; then
-  echo "$MV_MSG DELAY_IN_DAYS"
-  deactivate
-  exit 1
-fi
-
-if [ "$DB_ADDRESS" == "" ]; then
-  echo "$MV_MSG DB_ADDRESS"
-  deactivate
-  exit 1
-fi
+MODE="dev"
+process_arguments $@
 
 export PYTHONPATH=src:$PYTHONPATH
 pip3 install -r requirements.txt
-python3 src/main.py $1
+python3 src/main.py $MODE
 deactivate
